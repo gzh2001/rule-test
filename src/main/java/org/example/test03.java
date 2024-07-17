fb4c7ebb47f07d2c1a411c81d1d933b5b4312d2f
Gaofan <gzhdev@outlook.com>
2024-07-17 11:01:36 +0800

    案例测试-分隔符与废弃ip

---
+++ src/main/java/org/example/test03.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class test03 {
+    public static void main(String[] args) {
+        String host = "https://www.google.com";
+        String path = "search";
+        String query = host + "/" + path;
+
+
+    }
+}
