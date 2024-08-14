26b374f1d4b5ee092d447fbd88e7f1008835f648
Gaofan <gzhdev@outlook.com>
2024-08-14 17:28:09 +0800

    案例测试-wsdl

--- src/main/java/org/example/test3.java
+++ src/main/java/org/example/test3.java
@@ -2,10 +2,10 @@ package org.example;
 
 import javax.xml.namespace.QName;
 
-@WebService(targetNamespace="http://www.git.com")
-@XmlElementDecl(namespace = "http://www.git.com")
+@WebService(targetNamespace="http://www.git123.com")
+@XmlElementDecl(namespace = "http://www.git123.com")
 public class test3 {
-    private final static QName QNAME = new QName("http://www.git.com", "test3");
+    private final static QName QNAME = new QName("http://www.git123qname.com", "test3");
 
 
 }
