4e5668d8c84773e8dfcbd2c64f5eafef3ef1bfd0
Gaofan <gzhdev@outlook.com>
2024-08-21 16:03:50 +0800

    案例测试-wsdl

---
+++ src/main/java/org/example/test04_wsdl.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlSchema;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.namespace.QName;
+import javax.xml.ws.WebServiceClient;
+
+@WebService(targetNamespace="http://www.git1.com")
+@XmlElementDecl(namespace = "http://www.git2.com")
+@XmlType(namespace = "http://www.git3.com")
+@WebServiceClient(wsdlLocation = "http://www.git4.com")
+@WebParam(targetNamespace = "http://www.git5.com")
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.git3.com")
+@XmlSchema(namespace = "http://www.git3.com")
+public class test04_wsdl {
+    private final static QName QNAME = new QName("http://www.git123qname.com", "test3");
+
+
+}
