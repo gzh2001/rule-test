5fb9ab4f62cf4df76e240ac9b8bd4a2241485501
Gaofan <gzhdev@outlook.com>
2024-09-26 17:26:16 +0800

    案例测试-ip_only

--- src/main/java/org/example/test04_wsdl.java
+++ src/main/java/org/example/test04_wsdl.java
@@ -18,6 +18,23 @@ import javax.xml.ws.WebServiceClient;
 public class test04_wsdl {
 
     private final static QName QNAME = new QName("http://www.git123qname.com", "test3");
+    String pureIP1 = "192.168.1.1";
+    String pureIP2 = "10.0.0.5";
+
+    String ipWithPath1 = "192.168.1.1/path/to/resource";
+    String ipWithPath2 = "10.0.0.5/another/path";
+
+    String protocolIPPath1 = "http://192.168.1.1/path/to/resource";
+    String protocolIPPath2 = "https://10.0.0.5/another/path";
+
+    String website1 = "example.com";
+    String website2 = "testsite.org";
+
+    String websiteWithPath1 = "example.com/path/to/resource";
+    String websiteWithPath2 = "testsite.org/another/path";
+
+    String protocolWebsitePath1 = "http://example.com/path/to/resource";
+    String protocolWebsitePath2 = "https://testsite.org/another/path";
 
 
 }
